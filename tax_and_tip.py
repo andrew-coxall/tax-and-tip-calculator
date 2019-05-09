6f16e90d32b7af577ebb9d5b87c8a1ebdc8b3486
EC2 Default User <ec2-user@ip-172-31-90-150.ec2.internal>
2019-05-09 23:41:09 +0000

    got ride of comments

--- tax_and_tip.py
+++ tax_and_tip.py
@@ -24,18 +24,6 @@ if tip_yes_or_no == "yes":
     money_for_tip = float(tip_cost) * float(cost_of_food)
     print ("Your tip costs: ${:.2f}" .format(money_for_tip))
 
-    #All added up
-    #all_added_up = money_for_tip + money_for_tax + float(cost_of_food)
-    #print ("Your total costs: ${:.2f}" .format(all_added_up))
-    #print ("have a nice day")
-
-#else:
-#    all_added_up_no_tip = money_for_tax + float(cost_of_food)
-#    print ("You know I make less than minimum wage right")
-#    print ("Your bill comes to $")
-#    print (all_added_up_no_tip)
-
-    
 print ("Your tax costs: ${:.2f}" .format(money_for_tax))
 all_added_up = money_for_tip + money_for_tax + float(cost_of_food)
 print("")
